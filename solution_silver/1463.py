b529e85adc4d0614caf1cd5a2f8232d4cf4b80ad
wooseok <87642287+WanderedToLa@users.noreply.github.com>
2022-06-02 19:53:50 +0900

    Create 1463.py

---
+++ solution_silver/1463.py
@@ -0,0 +1,13 @@
+# n = int(input())
+# cnt = 0
+# second_cnt = 0
+# print(n // 2)
+
+# while 1 <= n:
+#     if n == 1:
+#         print(n)
+#         break
+#     if (n % 2 != 0) or (n % 3 != 0): #2와 3으로 나누어떨어지지 않을때
+#         n -= 1
+#     if n % 2 == 0:
+#         n = n // 2
