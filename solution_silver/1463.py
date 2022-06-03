c40dceef93d4cb817030b1231bb687730463d064
wooseok <87642287+WanderedToLa@users.noreply.github.com>
2022-06-03 17:48:27 +0900

    Update 1463.py

--- solution_silver/1463.py
+++ solution_silver/1463.py
@@ -11,3 +11,17 @@
 #         n -= 1
 #     if n % 2 == 0:
 #         n = n // 2
+
+n = int(input())
+d = [0] * 1000001
+
+for i in range(2, n+1):
+    d[i] = d[i-1] + 1
+    
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+
+print(d[n])
