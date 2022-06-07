eb0088e42c0511f0a7a24c73a248e50d35166f53
wooseok <87642287+WanderedToLa@users.noreply.github.com>
2022-06-07 17:51:48 +0900

    Update 2864.py

--- solution_bronze/2864.py
+++ solution_bronze/2864.py
@@ -1,1 +1,4 @@
-a , b = map(int , input().split())
+a , b = input().split()
+min = int(a.replace('6' , '5')) + int(b.replace('6' , '5'))
+max = int(a.replace('5' , '6')) + int(b.replace('5' , '6'))
+print(min , max)
